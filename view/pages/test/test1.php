328652b4d173a9466e459d701bbd063a3e9ed63f
Josue Alcivar <pideun@gmail.com>
2021-01-15 15:13:14 -0500

    master

--- view/pages/test/test1.php
+++ view/pages/test/test1.php
@@ -16,10 +16,34 @@
                         <input class="img" type="image" value="3" src="view/images/numeros/tres.png">
                     </div>
                 </div> -->
-                <div class="test-2 w-100">
-                    <h5 class="fs-3 text-secondary text-center">Seleccione la frase correcta</h5>
+                <!-- 
+                <div class="test-2 row justify-content-center">
+                    <div class="col-md-12">
+                        <h5 class="fs-2 m-3 text-secondary text-center">Marque la solucion correcta</h5>
+                    </div>
+                    <div class="col-md-4 align-self-center">
+                        <img class="w-75" src="view/images/numeros/dos.png" alt="">
+                    </div>
+                    <div class="col-md-4 align-self-center">
+                        <div class="w-100 check-answer form-check">
+                            <input type="radio" value="num-3" class="btn-check" id="numero-3">
+                            <label class="btn w-100" for="numero-3">Numero tres</label>
+                        </div>
+                        <div class="w-100 check-answer form-check">
+                            <input type="radio" class="btn-check" id="numero-2">
+                            <label class="btn w-100" for="numero-2">Numero dos</label>
+                        </div>
+                        <div class="w-100 check-answer form-check">
+                            <input type="radio" class="btn-check" id="numero-4">
+                            <label class="btn w-100" for="numero-4">Numero cuatro</label>
+                        </div>
+                        <div class="w-100 check-answer form-check">
+                            <input type="radio" class="btn-check" id="numero-1">
+                            <label class="btn w-100" for="numero-1">Numero uno</label>
+                        </div>
+                    </div>
+                </div> -->
 
-                </div>
             </div>
             <div class="col-md-12 buttons">
                 <button class="btn btn-success fs-4" id="next">Siguiente</button>
--- view/scripts/test.js
+++ view/scripts/test.js
@@ -2,6 +2,7 @@
 class MainTest {
 
     testOne;
+    testTwo;
     correctAnswer;
     progress = document.getElementById('progress-bar');
     currentProgress = 0;
@@ -13,6 +14,8 @@ class MainTest {
     loadTest(config) {
         if (config.typeTest == "img-select"){
             this.testOne = this.generateContentOne(config);
+        }else if(config.typeTest == "select-answer"){
+            this.testTwo = this.generateContentTwo(config);
         }
     }
 
@@ -38,6 +41,48 @@ class MainTest {
             let imgInput = `<input class="img" type="image" id="${testOne.images[i].name}" value="${testOne.images[i].value}" src="${testOne.images[i].file}">`;
             divChildren.innerHTML += imgInput;
         }
+        return divParent;
+    }
+
+    generateContentTwo(testTwo){
+        let divParent = document.createElement('div');
+        let divSonOne = document.createElement('div');
+        let divSonTwo = document.createElement('div');
+        let divSonOnThre = document.createElement('div');
+
+        let contentTitle = `<h5 class="fs-2 m-3 text-secondary text-center">${testTwo.titleTest}</h5>`;
+
+        divParent.classList.add('test-2');
+        divParent.classList.add('row');
+        divParent.classList.add('justify-content-center');
+
+        divSonOne.classList.add('col-md-12');
+        divSonOne.innerHTML = contentTitle;
+        divParent.appendChild(divSonOne);
+
+        divSonTwo.classList.add('col-md-4');
+        divSonTwo.classList.add('align-self-center');
+
+        let imgContent = `<img class="w-75" src="view/images/numeros/dos.png" alt="">`;
+        divSonTwo.innerHTML = imgContent;
+        divParent.appendChild(divSonTwo);
+
+        divSonOnThre.classList.add('col-md-4');
+        divSonOnThre.classList.add('align-self-center');
+        divSonOnThre.setAttribute('id','box-options');
+
+        for(let i=0; i< testTwo.options.length; i++){
+            let checkImput = `<div class="w-100 check-answer form-check">
+                <input type="radio" value="${testTwo.options[i].value}" class="btn-check" id="${testTwo.options[i].name}">
+                <label class="btn w-100" for="${testTwo.options[i].name}">${testTwo.options[i].title}</label>
+            </div>`;
+            divSonOnThre.innerHTML += checkImput;
+        }
+
+        divParent.appendChild(divSonOnThre);
+
+        this.correctAnswer = testTwo.answer;
+
         return divParent;
     }
 
@@ -77,29 +122,56 @@ class TestOne {
 }
 
 $('#test').ready(function(){
-    let config = {
+    var config = {
         typeTest: "img-select",
         answer: "img-2",
-        titleTest: "Cual de estos es 2?",
+        titleTest: "Cual de lo siguientes es 2",
         images: [{
             name: "numero-2",
             value: "img-2",
             file: "view/images/numeros/dos.png"
         },
         {
-            name: "numero-2",
+            name: "numero-4",
             value: "img-4",
             file: "view/images/numeros/cuatro.png"
         },
         {
-            name: "numero-2",
+            name: "numero-1",
             value: "img-1",
             file: "view/images/numeros/uno.png"
         },
         {
-            name: "numero-2",
+            name: "numero-3",
             value: "img-3",
             file: "view/images/numeros/tres.png"
+        }
+        ]
+    }
+
+    var configTwo = {
+        typeTest: "select-answer",
+        answer: "num-2",
+        titleTest: "Seleccione la respuesta correcta",
+        options: [{
+            title: "Numero dos",
+            name: "numero-2",
+            value: "num-2"
+        },
+        {
+            title: "Numero cuatro",
+            name: "numero-4",
+            value: "num-4"
+        },
+        {
+            title: "Numero uno",
+            name: "numero-1",
+            value: "num-1"
+        },
+        {
+            title: "Numero tres",
+            name: "numero-3",
+            value: "num-3"
         }
         ]
     }
@@ -108,22 +180,34 @@ $('#test').ready(function(){
     const testContent = document.getElementById('testContent');
     var onAnswer = "";
 
-    mainTest.loadTest(config);
-    testContent.appendChild(mainTest.testOne);
+    mainTest.loadTest(configTwo);
+    testContent.appendChild(mainTest.testTwo);
 
     var boxContent = document.getElementById('box-images');
+    var optionsContent = document.getElementById('box-options');
     var nextButton = document.getElementById('next');
 
-    function addEvent(element){
-        let listElement = element.childNodes;
-        listElement.forEach(index => {
-            index.addEventListener("click", function(){
-                onAnswer = index.value;
+    function addEvents(){
+        let listElement;
+        if(boxContent != null){
+            listElement = boxContent.childNodes;
+            listElement.forEach(index => {
+                index.addEventListener("click", function(){
+                    onAnswer = index.value;
+                });
             });
-        });
+        }else if(optionsContent != null) {
+            listElement = $("#box-options input");
+            for (let i = 0; i< listElement.length; i++){
+                listElement[i].addEventListener("click", ()=>{
+                    onAnswer = listElement[i].value;
+                });
+            }
+        }
+
     }
 
-    addEvent(boxContent);
+    addEvents();
     nextButton.addEventListener('click',()=>{
         mainTest.checkAnswer(mainTest.correctAnswer, onAnswer);
     });
