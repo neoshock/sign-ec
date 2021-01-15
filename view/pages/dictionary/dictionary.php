bb760198376424fc098e14ce364f86cdae5196d2
otherluis <ldelacruzg@uteq.edu.ec>
2021-01-15 15:09:42 -0500

    master

--- view/pages/dictionary/dictionary.php
+++ view/pages/dictionary/dictionary.php
@@ -4,13 +4,13 @@
         <p class="text-center">Ver significados de palabras en lenguaje de señas ecuatoriano</p>
         <div class="input-group">
             <span class="input-group-text">@</span>
-            <input type="text" id="lds_search_word" class="form-control" placeholder="Escribe una palabra">
+            <input type="text" id="lds_search_word" class="form-control" placeholder="Escribe una palabra" autocomplete="off">
         </div>
         <ul class="list-group result-search-word">
             <!--<li class="list-group-item">Uno</li>-->
         </ul>
 
-        <div class="card mb-3 hide" style="margin-top: 10px">
+        <div class="card mb-3 d-none" style="margin-top: 10px">
             <div class="row g-0">
                 <div class="col-md-4">
                     <img class="img-fluid" src="view/images/numeros/uno.png" alt="...">
@@ -19,11 +19,16 @@
                     <div class="card-body">
                     <h5 class="card-title">Uno</h5>
                     <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
-                    <button class="btn btn-success">Ver video</button>
+                    <button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#modal-video">
+                        Ver video
+                    </button>
                     </div>
                 </div>
             </div>
         </div>
     </div>
 </div>
+
+<div class="myModal"></div>
+
 
--- view/pages/dictionary/script.js
+++ view/pages/dictionary/script.js
@@ -12,22 +12,60 @@ $("#lds_search_word").keyup(function(e){
             $.each(data, function(index, obj){
                 if (obj.word.toLowerCase().includes(element.val().toLowerCase()))
                 {
-                    html += '<li class="list-group-item">'+obj.word+'</li>';
+                    html += '<li class="list-group-item" data-word="'+obj.word+'" data-image="'+obj.image+'" data-video="'+obj.video+'" data-description="'+obj.description+'">'+obj.word+'</li>';
                 }
             });
             $(".result-search-word").append(html);
         });
 
-        $("li").on("click", function(){
-            console.log("mostrar informacion del item seleccionado")
-            element.val($(this).text());
-            $(".list-group").html("");
+        $(".list-group li").on("click", function(){
+            var item = $(this), card = $(".card");
+            // colocar valor al campo
+            element.val(item.text());
+            // limpiar lista
+            item.parent().html("");
+            // mostrar contenido
+            card.removeClass("d-none");
+            $(".card img").attr("src", item.attr("data-image"));
+            $(".card-title").html(item.attr("data-word"));
+            $(".card-text").html(item.attr("data-description"));
+            $(".myModal").html(showModalVideo({
+                title: item.attr("data-word"),
+                video: item.attr("data-video")
+            }));
+            
         });
     }else{
         element.parent().siblings('ul').html('');
+        $(".card").addClass("d-none");
     }
 });
 
+var showModalVideo = function(config)
+{
+    var html = "";
+    html += '<div class="modal fade" id="modal-video" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">';
+    html += '  <div class="modal-dialog modal-dialog-centered modal-lg text-center">';
+    html += '    <div class="modal-content">';
+    html += '      <div class="modal-header">';
+    html += '        <h5 class="modal-title" id="exampleModalLabel">'+config.title+'</h5>';
+    html += '        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>';
+    html += '      </div>';
+    html += '      <div class="modal-body">';
+    html += '        <div class="container">';
+    html += '          <div class="row">';
+    html += '            <iframe src="'+config.video+'" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>';
+    html += '          </div>';
+    html += '        </div>';
+    html += '      </div>';
+    html += '      <div class="modal-footer">';
+    html += '        <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>';
+    html += '      </div>';
+    html += '    </div>';
+    html += '  </div>';
+    html += '</div>';
+    return html;
+}
 
 
 
